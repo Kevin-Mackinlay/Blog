1356164044a9279616db3aef5a48248bd2e5f0af
Mackinlay Kevin <kemack83@gmail.com>
2024-07-03 22:25:57 -0300

    Your commit message

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Logo from '../img/logo.png';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../context/authContext';
 
 const Navbar = () => {
+  const { currentUser, logout } = useContext(AuthContext);
+
   return (
     <div className="navbar">
       <div className="container">
@@ -33,8 +36,8 @@ const Navbar = () => {
           <Link className="link" to="/?cat=art">
             <h6>ART</h6>
           </Link>
-          <span>Mack</span>
-          <span>Logout</span>
+          <span>{currentUser?.username }</span>
+          {currentUser ? <span onClick={logout}>Logout</span> : <Link className='link' to="/login" >Login</Link>}
           <span className='write'> <Link className='link' to="/write">Write</Link></span>
         </div>
       </div>
