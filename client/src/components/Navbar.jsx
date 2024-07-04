ee1f0959522c2712b6c30b88fbf38eb6cf70c020
Mackinlay Kevin <kemack83@gmail.com>
2024-07-04 17:29:31 -0300

    changes in single page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,7 +10,10 @@ const Navbar = () => {
     <div className="navbar">
       <div className="container">
         <div className="logo">
-          <img src={Logo} alt="" />
+          <Link to="/">
+            <img src={Logo} alt="" />
+          </Link>
+          
         </div>
         <div className="links">
           <Link className="link" to="/?cat=trips">
@@ -36,9 +39,20 @@ const Navbar = () => {
           <Link className="link" to="/?cat=art">
             <h6>ART</h6>
           </Link>
-          <span>{currentUser?.username }</span>
-          {currentUser ? <span onClick={logout}>Logout</span> : <Link className='link' to="/login" >Login</Link>}
-          <span className='write'> <Link className='link' to="/write">Write</Link></span>
+          <span>{currentUser?.username}</span>
+          {currentUser ? (
+            <span onClick={logout}>Logout</span>
+          ) : (
+            <Link className="link" to="/login">
+              Login
+            </Link>
+          )}
+          <span className="write">
+            {' '}
+            <Link className="link" to="/write">
+              Write
+            </Link>
+          </span>
         </div>
       </div>
     </div>
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,33 +1,69 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Edit from '../img/editar.png';
 import Delete from '../img/borrar.png';
-import {Link} from 'react-router-dom';
+import { Link, Navigate, useLocation, useNavigate } from 'react-router-dom';
 import Menu from '../components/Menu';
+import axios from 'axios';
+import moment from 'moment';
+import { AuthContext } from '../context/authContext';
 
 const Single = () => {
+  const [post, setPost] = useState({});
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const postId = location.pathname.split('/')[2];
+
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/${postId}`);
+        setPost(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [postId]);
+
+  const handleDelete = async () => {
+
+ try {
+        await axios.delete(`/posts/${postId}`);
+      Navigate('/');
+      } catch (err) {
+        console.log(err);
+      }
+  }
+
+
   return (
     <div className="single">
       <div className="content">
-        <img src="https://images.pexels.com/photos/2350074/pexels-photo-2350074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="single" />
+        <img src={post?.img} />
         <div className="user">
-          <img src="https://images.pexels.com/photos/3376116/pexels-photo-3376116.jpeg?auto=compress&cs=tinysrgb&w=400" alt="" />
+          {post.userImg  && <img src={post.userImg} alt="" />}
           <div className="info">
-            <span>Paula</span>
-            <p>Posted 2 days ago</p>
+            <span>{post.username}</span>
+            <p>Posted {moment(post.date).fromNow} </p>
           </div>
-          <div className="edit">
-            <Link to={`/write?edit=2`}>
-              <img src={Edit} alt="" />
-            </Link>
-            <img src={Delete} alt="" />
-          </div>
+          {currentUser.username === post.username && (
+            <div className="edit">
+              <Link to={`/write?edit=2`}>
+                <img src={Edit} alt="" />
+              </Link>
+              <img onClick={handleDelete} src={Delete} alt="" />
+            </div>
+          )}
         </div>
-        <h1>Lorem ipsum dolor.</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis neque voluptates officia ad hic aspernatur rerum quaerat, tempora soluta nam sequi nulla dolor unde placeat incidunt enim tenetur accusantium inventore!.
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, beatae adipisci. Repellat maxime ad doloremque, ab tenetur quas veritatis cumque, officiis eveniet modi, at eum enim eligendi. Quisquam, eum voluptatum! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa ut aperiam nobis doloribus iure, atque laudantium. Corporis deserunt, atque molestiae voluptatum aliquid odit praesentium sunt fugiat. Suscipit ullam vitae quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta rerum eos ratione ea obcaecati quos, magni voluptates architecto unde ut saepe aut enim laudantium labore dolorum nihil ab quam sequi. Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-         Alias, iusto vel ratione repudiandae dolorum perspiciatis pariatur autem, impedit provident dignissimos ea eos tempore ipsum. Officiis ullam eligendi nostrum. Temporibus, voluptas.</p>
+        <h1>{post.title}</h1>
+
+        {post.desc}
       </div>
-     <Menu />
+      <Menu />
     </div>
   );
 };
